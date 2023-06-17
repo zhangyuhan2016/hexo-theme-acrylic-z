801695431d915a8a6b7818382fd841886e484f38
张雨涵 <zhangyuhan.hello@gmail.com>
2023-06-17 15:45:23 +0800

    feat: 扩展支持案例展示、样式优化

--- scripts/filter/cdn.js
+++ scripts/filter/cdn.js
@@ -1,6 +1,12 @@
 const cheerio = require('cheerio');
 
 hexo.extend.filter.register('after_render:html', function (str) {
+
+    const isDev = hexo.env.env === 'development'
+    if (isDev) {
+        return str
+    }
+
     const $ = cheerio.load(str, { decodeEntities: false });
 
     const cdnPrefixAssets = hexo.theme.config.cdn?.prefix?.assets;
@@ -9,11 +15,15 @@ hexo.extend.filter.register('after_render:html', function (str) {
     if (cdnPrefixAssets || cdnPrefixImg) {
         $('link[rel="stylesheet"], script[src], img[src]').each(function () {
             const tag = $(this).is('link') ? 'link' : $(this).is('script') ? 'script' : 'img';
-            const url = $(this).attr('src');
+            const replaceKey = tag === 'link' ? 'href' : 'src'
+            const url = $(this).attr(replaceKey);
 
             if (url && (!url.startsWith('http://') && !url.startsWith('https://') && !url.startsWith('//') && !url.startsWith('data:'))) {
                 const newUrl = tag === 'img' ? (cdnPrefixImg + url) : (cdnPrefixAssets + url);
-                $(this).attr('src', newUrl);
+                if (!isDev) {
+                    console.log(`[cdn replace]: \n  ${tag}\n    ${url}\n    =>  ${newUrl}`)
+                }
+                $(this).attr(replaceKey, newUrl);
             }
         });
     }
